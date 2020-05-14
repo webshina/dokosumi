38e4313da9a7ff074d03d5facf375208852ff407
kazukiyamashina <kazu4101719@gmail.com>
2020-05-15 00:24:53 +0900

    commit

--- dokosumi_app/management/commands/autoPost.py
+++ dokosumi_app/management/commands/autoPost.py
@@ -26,7 +26,7 @@ class Command(BaseCommand):
         twitter_post_comment += '治安の良さ : ' + str(int(sample.security)) + ' 点\n'
         twitter_post_comment += '緑地の多さ : ' + str(int(sample.park)) + ' 点\n'
         twitter_post_comment += '浸水危険度の低さ : ' + str(int(sample.flood)) + ' 点\n'
-        twitter_post_comment += 'https://dokosumi.stuply.com/town_detail/' + urllib.parse.quote(sample.station_name) + '/\n'
+        twitter_post_comment += 'https://dokosumi.stuply.com/town_detail/' + urllib.parse.quote(sample.station_name) + '\n'
         print(twitter_post_comment)
 
         # 画像取得
