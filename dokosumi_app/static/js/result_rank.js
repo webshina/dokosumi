327df9b51a522db1a11daec0af4466da1db1ee52
kazukiyamashina <kazu4101719@gmail.com>
2020-11-15 20:47:00 +0900

    commit

--- dokosumi_app/static/js/result_rank.js
+++ dokosumi_app/static/js/result_rank.js
@@ -11,7 +11,7 @@ param = param.replace('%3F','?');
 // 各SNSのシェアURLと、シェアしたいURL、パラメータ、ハッシュタグを結合
 var twLink_t = document.getElementById('twitter-link-btn').href
 console.log(twLink_t)
-twLink = twLink_t + url + param + '&hashtags=DOKOSUMIYOSHI,どこ住吉'
+twLink = twLink_t + url + param + '&hashtags=どこ住吉,住みよい街,引っ越し'
 
 // シェアボタンのリンクを置き換える
 $('#twitter-link-btn').attr('href', twLink);
