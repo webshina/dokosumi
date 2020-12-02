88ee57233a79446a18c3f7a5e906b992281ce802
kazukiyamashina <kazu4101719@gmail.com>
2020-12-03 00:43:10 +0900

    commit

--- dokosumi_app/management/commands/autoPost.py
+++ dokosumi_app/management/commands/autoPost.py
@@ -35,7 +35,7 @@ class Command(BaseCommand):
         twitter_post_comment += '治安の良さ : ' + str(int(sample.security)) + ' 点\n'
         twitter_post_comment += '公園の多さ : ' + str(int(sample.park)) + ' 点\n'
         twitter_post_comment += '浸水危険度の低さ : ' + str(int(sample.flood)) + ' 点\n\n'
-        twitter_post_comment += '#どこ住吉\n\n'
+        twitter_post_comment += '#どこ住吉\n'
         twitter_post_comment += 'https://dokosumiyoshi.tokyo/town_detail/' + urllib.parse.quote(sample.station_name) + '\n'
         print(twitter_post_comment)
 
