b57d2c68d82d7da555057cf91bfdc84e2a3316f5
kazukiyamashina <kazu4101719@gmail.com>
2021-01-07 21:56:17 +0900

    commit

--- tools/git_pull.sh
+++ tools/git_pull.sh
@@ -7,6 +7,6 @@ python ./manage.py makemigrations
 python ./manage.py migrate
 python ./manage.py collectstatic --noinput
 
-#sed -i -e "s/DEBUG = True/DEBUG = False/g" ./dokosumi/settings.py
+sed -i -e "s/DEBUG = True/DEBUG = False/g" ./dokosumi/settings.py
 
 sudo service httpd restart
